cd2f55310922986d784a88ba2dbf1d1e90977f8a
amatkar <amol.matkar@blancco.com>
2020-05-07 17:57:55 +0530

    Updates

---
+++ precache-manifest.54b4964401d23968e61e228a1a78e364.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79e7f12d038c66e168e8a978fc78f6b",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "4c3bee20cea00d7a028b",
+    "url": "/contactmanager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "4368ac84e815d7d4c582",
+    "url": "/contactmanager/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "4c3bee20cea00d7a028b",
+    "url": "/contactmanager/static/js/2.74c5206a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.74c5206a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4368ac84e815d7d4c582",
+    "url": "/contactmanager/static/js/main.04a43a50.chunk.js"
+  },
+  {
+    "revision": "daeb1d9a0e761e02937e",
+    "url": "/contactmanager/static/js/runtime-main.87683053.js"
+  }
+]);
